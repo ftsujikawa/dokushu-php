24ec15465c0a9426abed6e251ab1aad923260c36
ftsujikawa <readme627@gmail.com>
2020-03-23 18:50:52 +0900

    練習問題2.3終了

---
+++ chap02/ch02-ren22.php
@@ -0,0 +1,8 @@
+<?php
+$DATA = 1;
+//$123 = 2;
+$X山田 = 3;
+
+print $DATA;
+//print $123;
+print $X山田;
---
+++ chap02/ch02-ren231.php
@@ -0,0 +1,8 @@
+<?php
+//const 1A = 1;
+const data = 2;
+//const A+1 = 3;
+
+//print 1A;
+print data;
+//print A+1;
