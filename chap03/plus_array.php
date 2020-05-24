4b9d1a3bc41f21f04f9c7d1b35c013ba73d60544
ftsujikawa <readme627@gmail.com>
2020-05-24 13:52:37 +0900

    練習問題３．１終了

---
+++ chap03/plus_array.php
@@ -0,0 +1,18 @@
+<?php
+$ascoc1 = [
+  'Apple' => 'Red',
+  'Orange' => 'Yellow',
+  'Melon' => 'Green'
+];
+$assoc2 = [
+  'Grape' => 'Purple',
+  'Apple' => 'Green',
+  'Strawberry' => 'Red'
+];
+$result = $ascoc1 + $assoc2;
+print_r($result);
+
+$ary1 = [1, 3, 5];
+$ary2 = [2, 3, 6];
+$result = $ary1 + $ary2;
+print_r($result);
