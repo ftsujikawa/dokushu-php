a1ebb9d3b427b63180f1274666536eb89dc557c3
ftsujikawa <readme627@gmail.com>
2020-05-25 13:50:14 +0900

    練習問題3.2終了

---
+++ chap03/ch03-ren321.php
@@ -0,0 +1,4 @@
+<?php
+$x = 2;
+$flag = $x == 1 ? 0 : -1;
+print $flag;
---
+++ chap03/ch03-ren322.php
@@ -0,0 +1,5 @@
+<?php
+var_dump('1.44E2' == 144);
+var_dump('1.44E2' === 144);
+var_dump('0x10' == '16');
+var_dump('H' == 0);
